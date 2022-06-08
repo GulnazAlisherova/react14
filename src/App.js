80e94343c72ca5c81d609799d063561367e4e425
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2022-06-08 15:01:17 +0600

    removed

--- src/App.js
+++ src/App.js
@@ -1,36 +1,41 @@
-// import { useEffect, useState } from "react";
+
 import ExchangeRate from "./components/ExchangeRate";
 
 function App() {
-//   const [number, setNumber] = useState(0);
-
-//   useEffect(() => {
-//     alert('Hello');
-//   });
-
-// //side effect
-// //fetch - ajax
-// //styling
-
-//   const color = {
-//     red: Math.floor(Math.random() * 255),
-//     green: Math.floor(Math.random() * 255),
-//     blue: Math.floor(Math.random() * 255),
-
-//   };
-
-//   const styles = {
-//     color: `rgb(${color.red}), ${color.green}, ${color.blue}`,
-//   }
-
   return (
     <div className="App">
-      {/* <h1 style={styles}>Hello world</h1>
-      <span>{number}</span>
-      <button onClick={() => setNumber(number + 1)}>+</button> */}
       <ExchangeRate from="USD" to="KGS"/>
+      <ExchangeRate from="EUR" to="KGS"/>
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
+
+// import { useEffect, useState } from "react";
+//   const [number, setNumber] = useState(0);
+//   useEffect(() => {
+//     alert('Hello');
+//   });
+// //side effect
+// //fetch - ajax
+// //styling
+//   const color = {
+//     red: Math.floor(Math.random() * 255),
+//     green: Math.floor(Math.random() * 255),
+//     blue: Math.floor(Math.random() * 255),
+//   };
+//   const styles = {
+//     color: `rgb(${color.red}), ${color.green}, ${color.blue}`,
+//   }
+
+   {/* <h1 style={styles}>Hello world</h1>
+      <span>{number}</span>
+      <button onClick={() => setNumber(number + 1)}>+</button> */}
