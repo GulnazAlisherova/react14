826e7c82f6b827b3407352a6699976bfd9768ea2
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2022-06-08 14:37:09 +0600

    added new changes

---
+++ src/components/ExchangeRate.js
@@ -0,0 +1,10 @@
+function ExchangeRate(props) {
+  const rate = 89
+  return ( 
+    <div className="ExchangeRate">
+     {props.from}/{props.to} = {rate}
+    </div>
+   );
+}
+
+export default ExchangeRate;
